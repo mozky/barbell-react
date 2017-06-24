54ea2bb0748030128a7ecdb642cb60b888e2cb58
Moz <javiersuarezl@outlook.com>
2017-06-23 23:12:19 -0500

    Simple.. React

--- app/store/configureStore.js
+++
@@ -1,7 +0,0 @@
-import config from '../../config';
-
-if (config.env === 'production') {
-    module.exports = require('./configureStore.prod');
-} else {
-    module.exports = require('./configureStore.dev');
-}
--- config.js
+++
@@ -1,6 +0,0 @@
-const config = {};
-
-config.env = process.env.NODE_ENV || 'development';
-config.port = process.env.WEB_PORT || 3000;
-
-module.exports = config;
