93c6b3da76eb2a0e175cc1bd6c90481ef9549c30
Moz <javiersuarezl@outlook.com>
2017-07-16 19:51:00 -0500

    adds custom option

--- src/components/RoutineCreator/SearchExercise.js
+++ src/components/RoutineCreator/SearchExercise.js
@@ -1,6 +1,54 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
+
+class ExerciseOption extends Component {
+  constructor(props) {
+    super(props)
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+
+	handleMouseDown (event) {
+		event.preventDefault();
+		event.stopPropagation();
+		this.props.onSelect(this.props.option, event);
+	}
+
+	handleMouseEnter (event) {
+		this.props.onFocus(this.props.option, event);
+	}
+
+	handleMouseMove (event) {
+		if (this.props.isFocused) return;
+		this.props.onFocus(this.props.option, event);
+	}
+
+	render () {
+		return (
+			<div className={this.props.className}
+				onMouseDown={this.handleMouseDown}
+				onMouseEnter={this.handleMouseEnter}
+				onMouseMove={this.handleMouseMove}>
+        {/* USE THIS TO CUSTOMIZE OPTION TO SHOW EXTRA DATA */}
+        {this.props.children} - ({this.props.option.value})
+			</div>
+		);
+	}
+}
+
+ExerciseOption.PropTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  isFocused: PropTypes.bool,
+  isSelected: PropTypes.bool,
+  onFocus: PropTypes.func,
+  onSelect: PropTypes.func,
+  option: PropTypes.object.isRequired,
+}
 
 export default class SearchExercise extends Component {
   constructor(props) {
@@ -17,9 +65,12 @@ export default class SearchExercise extends Component {
   render() {
     return (
       <Select
+        style={{'textAlign': 'center'}}
+        placeholder="Add new exercise"
         name="form-field-name"
         value={null}
         options={this.props.exercises}
+        optionComponent={ExerciseOption}
         onChange={this.handleSearchBoxChange}
       />
     )
